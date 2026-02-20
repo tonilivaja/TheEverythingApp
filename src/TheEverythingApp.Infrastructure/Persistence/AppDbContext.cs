c6491a18bfebeaf1650d9dc0f6f47bd67c7ae7e3
Toni Livaja <livajatenja@gmail.com>
2026-02-20 19:10:54 +0100

    Set up dependencies between projects, added required NuGet packages

---
+++ src/TheEverythingApp.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TheEverythingApp.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        // To do
+    }
+}
--- src/TheEverythingApp.WebAPI/Program.cs
+++ src/TheEverythingApp.WebAPI/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TheEverythingApp.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DB context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 // Add services to the container.
 builder.Services.AddControllers();
